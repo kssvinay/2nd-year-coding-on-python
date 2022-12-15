77b5e2028b7d44d1fab13ec98e011fbe38765838
kssvinay <110870903+kssvinay@users.noreply.github.com>
2022-12-15 09:04:12 +0530

    Create 14_12_2022_min_max.py

---
+++ 14_12_2022_min_max.py
@@ -0,0 +1,12 @@
+my_list = [-15, -26, 15, 1, 23, -64, 23, 76]
+new_list = []
+
+while my_list:
+    min = my_list[0]  
+    for x in my_list: 
+        if x < min:
+            min = x
+        If x > max:
+              Max=x
+Print(min)
+Print(max)
