db99bde04d76d178064415db23fadc4c286797c0
kssvinay <110870903+kssvinay@users.noreply.github.com>
2022-12-28 05:41:28 +0530

    Update 27_12_2022_el_f_o_nf.py

--- 27_12_2022_el_f_o_nf.py
+++ 27_12_2022_el_f_o_nf.py
@@ -2,6 +2,10 @@ n = int(input())
 l = list(map(int,input().split()))
 s = int(input())
 for s in l:
-    print("element found")
+    f = 1
 else:
+    f=0
+if f==0:
     print("element not found")
+else:
+    print("element found")
