b0040e09a674c7e061bd39adf917524387b108c2
kssvinay <110870903+kssvinay@users.noreply.github.com>
2023-05-02 06:18:14 +0530

    Create 02_05_2023_big_of_3_num.py

---
+++ 02_05_2023_big_of_3_num.py
@@ -0,0 +1,10 @@
+
+a = int(input("Enter NUmber 1: "))
+b = int(input("Enter Number 2: "))
+c = int(input("Enter Number 3: "))
+if(a>b and a>c):
+    print("A is big of values", a)
+elif(b>a and b>c):
+    print("B is big of values", b)
+else:
+    print("C is big of values", c)
