5358fc2fae3c22322cf79b74b4bb84a41a03663a
kssvinay <110870903+kssvinay@users.noreply.github.com>
2023-05-06 08:47:07 +0530

    Create 05_05_23_primes_count_sieve.py

---
+++ 05_05_23_primes_count_sieve.py
@@ -0,0 +1,14 @@
+seive=[]
+n=int(input())
+n=n+1
+for i in range(0,n):
+    seive.append(1)
+for i in range(2,int(n**(0.5))+1):
+    if seive[i]==1:
+        for j in range(i*i,n,i):
+            seive[j]=0
+count = 0
+for i in range(2,n):
+    if seive[i]==1:
+        count += 1
+print(count,end=' ')
