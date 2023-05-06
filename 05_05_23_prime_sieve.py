4a6684bb980ffd97d8f6f63c863fef00d56fcb55
kssvinay <110870903+kssvinay@users.noreply.github.com>
2023-05-06 08:44:41 +0530

    Update 05_05_23_prime_sieve.py

--- 05_05_23_prime_sieve.py
+++ 05_05_23_prime_sieve.py
@@ -3,7 +3,7 @@ n=int(input())
 n=n+1
 for i in range(0,n):
     seive.append(1)
-for i in range(2,(n**(0.5))+1):
+for i in range(2,int(n**(0.5))+1):
     if seive[i]==1:
         for j in range(i*i,n,i):
             seive[j]=0
